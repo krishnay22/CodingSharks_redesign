2e8d6172b7a47b88d2fef534ac6cda1bef7e3dbe
Krishna yadav <krishnayadav224466@gmail.com>
2025-03-20 13:28:17 +0530

    added review slider

--- codings/frontend/src/pages/landing/Home.js
+++ codings/frontend/src/pages/landing/Home.js
@@ -6,23 +6,26 @@ import EnquirySection from "../../components/homecomponents/Enquire";
 import CommunitySection from "../../components/homecomponents/Aboutus";
 import CustomVerticalScrollbar from "../../components/homecomponents/teachingMethods";
 import NFTCard from "../../components/homecomponents/cards";
+import ReviewSlider from "../../components/homecomponents/reviews";
 const Home = () => {
   return (
     <>
       <div style={{ width: "100%", overflow: "hidden" }}>
         <EnquirySection />
         <div style={{ margin: "180px 0px 100px 0px" }}>
-          <LogoSlider />
-        </div>
-        <div style={{ margin: "180px 0px 100px 0px" }}>
           <CommunitySection />
         </div>
+        <div style={{ margin: "180px 0px 100px 0px" }}>
+          <LogoSlider />
+        </div>
+
         <div style={{ margin: "180px 0px 100px 0px" }}>
           <CustomVerticalScrollbar />
         </div>
         <div style={{ margin: "180px 0px 100px 0px" }}>
           <NFTCard />
         </div>
+        <ReviewSlider />
       </div>
     </>
   );
