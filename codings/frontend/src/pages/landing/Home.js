2c5f29e75cbea7894fe65889506edc7b8e874b47
Krishna yadav <krishnayadav224466@gmail.com>
2025-03-31 13:09:09 +0530

    working on homepage

--- codings/frontend/src/pages/landing/Home.js
+++ codings/frontend/src/pages/landing/Home.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LogoSlider from "../../components/homecomponents/logoslider";
 import EnquirySection from "../../components/homecomponents/Enquire";
 import CommunitySection from "../../components/homecomponents/Aboutus";
-import NFTCard from "../../components/homecomponents/cards";
 import ReviewSlider from "../../components/homecomponents/ReviewSlider";
 import RecruitmentStories from "../../components/homecomponents/Slider";
 import Footer from "../../components/homecomponents/FooterForLandingpage";
 
 const Home = () => {
-  const [offset, setOffset] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setOffset(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="w-full overflow-x-hidden">
       <EnquirySection />
@@ -27,21 +14,8 @@ const Home = () => {
       <div className="my-32">
         <CommunitySection />
       </div>
-
-      {/* Parallax NFT Card Over LogoSlider */}
-      <div className="relative w-full h-screen overflow-hidden">
-        <div
-          style={{
-            transform: `translateY(${offset}px)`,
-            transition: "transform 0.1s ease-out",
-          }}
-        >
-          <LogoSlider />
-        </div>
-
-        <div>
-          <NFTCard />
-        </div>
+      <div>
+        <LogoSlider />
       </div>
 
       <div className="my-32">
